146fc13c03360cac70174d86e3f1e161e00bf042
Thotakura Sri Venkatesh <da25m530@e2e-73-84.ssdcloudindia.net>
2026-08-20 17:35:55 +0530

    Phase 3 : Model Training

--- src/data_ingestion/fraud_pipeline_dag.py
+++ src/data_ingestion/fraud_pipeline_dag.py
@@ -34,32 +34,36 @@ with DAG(
     # TASK 3: Feature Engineering & Preprocessing
     # ---------------------------------------------------------
     # Cleans data, applies SMOTE, scales features, and saves the processed dataset
+  
     # run_preprocessing = BashOperator(
     #      task_id='run_preprocessing',
-    #      bash_command='docker exec spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-cores 2 --executor-memory 4g --driver-memory 1g --conf spark.driver.cores=1 --conf spark.driver.maxResultSize=1g --conf spark.executor.memoryOverhead=384 --conf spark.memory.fraction=0.600 --conf spark.memory.storageFraction=0.500 --conf spark.sql.shuffle.partitions=16 --conf spark.default.parallelism=12 --conf spark.sql.adaptive.advisoryPartitionSizeInBytes=128MB --conf spark.sql.adaptive.enabled=true --conf spark.jars.ivy=/tmp/.ivy2 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0 --name team32_preprocess_19thAug-da25m530 /storage/uploads/Team32_FraudDetection/src/features/preprocessSpark.py')
-    run_preprocessing = BashOperator(
-         task_id='run_preprocessing',
-         bash_command='docker exec spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-cores 2 --executor-memory 4g --driver-memory 1g --conf spark.driver.cores=1 --conf spark.driver.maxResultSize=1g /storage/uploads/Team32_FraudDetection/src/features/preprocessSpark.py'
-    )
-    # run_preprocessing = BashOperator(
-    #         task_id="run_preprocessing",
-    #         bash_command=(
-    #             "docker exec spark-master /opt/spark/bin/spark-submit "
-    #             "--master spark://spark-master:7077 "
-    #             "--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0 "
-    #             "/storage/uploads/Team32_FraudDetection/src/features/preprocess.py"
-    #         )
+    #      bash_command='docker exec spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-cores 2 --executor-memory 4g --driver-memory 1g --conf spark.driver.cores=1 --conf spark.driver.maxResultSize=1g /storage/uploads/Team32_FraudDetection/src/features/preprocessSpark.py'
+    # )
+    
     # ---------------------------------------------------------
     # TASK 4: Model Training & MLflow Logging
     # ---------------------------------------------------------
     # # Trains the model on the processed data and logs metrics to MLflow
-    # run_training = BashOperator(
-    #     task_id='run_model_training',
-    #     bash_command='python src/models/train.py',
-    # )
+   run_training = BashOperator(
+        task_id='run_model_training',
+        bash_command=(
+            'docker exec '
+            '-e MLFLOW_TRACKING_URI="$MLFLOW_TRACKING_URI" '
+            '-e MLFLOW_EXPERIMENT_NAME=team32_fraud_detection '
+            'spark-master /opt/spark/bin/spark-submit '
+            '--master spark://spark-master:7077 '
+            '--total-executor-cores 2 '
+            '--executor-cores 2 '
+            '--executor-memory 4g '
+            '--driver-memory 1g '
+            '--conf spark.driver.cores=1 '
+            '--conf spark.driver.maxResultSize=1g '
+            '/storage/uploads/Team32_FraudDetection/src/models/train.py'
+        )
+    )
 
     # ---------------------------------------------------------
     # Define Task Dependencies (Execution Order)
     # ---------------------------------------------------------
     #run_producer >> run_consumer >> run_preprocessing >> run_training
-    run_preprocessing
+    run_training
